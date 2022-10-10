8d62db589d30392526a05bb6eea1e172b343746f
Bob Nadler <bnadlerjr@gmail.com>
2022-10-10 11:41:52 -0400

    Ring middleware for logging request start info

--- src/com/bobnadler/toolkit.clj
+++ src/com/bobnadler/toolkit.clj
@@ -1,5 +1,7 @@
 (ns com.bobnadler.toolkit
-  "Various utility functions for Clojure projects.")
+  "Various utility functions for Clojure projects."
+  (:require
+   [clojure.string :as s]))
 
 (defn wrap-request-id
   "Ring middleware that wraps a request with an identifier.
@@ -21,3 +23,26 @@
 ; {:headers {"x-request-id" "c9bd7379-3fe9-45e8-8f69-a8e20fa007a7"},
 ;  :request-id "c9bd7379-3fe9-45e8-8f69-a8e20fa007a7"}
   )
+
+(defn- default-log-fn [{:keys [level msg attrs]}]
+  (.println System/out (format "%s | %s -- %s" (s/upper-case (name level)) msg attrs)))
+
+(defn wrap-request-start-logger
+  "Ring middleware that logs information about the start of the request."
+  [handler & {:keys [log-fn] :or {log-fn default-log-fn}}]
+  (fn [request]
+    (let [method (s/upper-case (name (:request-method request)))
+          path (str (:uri request)
+                    (when-let [query-string (:query-string request)]
+                      (str "?" query-string)))]
+      (log-fn {:level :info
+               :msg (format "Started %s '%s'" method path)
+               :attrs {:method method :path path}})
+      (handler request))))
+
+(comment
+  (def handler (wrap-request-start-logger identity))
+
+  (handler {:request-method :get
+            :uri "/foo"
+            :query-string "bar=2"}))
--- test/com/bobnadler/toolkit_test.clj
+++ test/com/bobnadler/toolkit_test.clj
@@ -1,9 +1,9 @@
 (ns com.bobnadler.toolkit-test
   (:require [clojure.test :as t]
-            [com.bobnadler.toolkit :as btk]))
+            [com.bobnadler.toolkit :as bntk]))
 
 (t/deftest wrap-request-id-test
-  (let [handler (btk/wrap-request-id identity)]
+  (let [handler (bntk/wrap-request-id identity)]
 
     (t/testing "no header"
       (t/is (contains? (handler {}) :request-id)))
@@ -12,3 +12,13 @@
       (let [req-id "c9bd7379-3fe9-45e8-8f69-a8e20fa007a7"]
         (t/is (= req-id
                  (:request-id (handler {:headers {"x-request-id" req-id}}))))))))
+
+(t/deftest wrap-request-start-logger-test
+  (let [state_ (atom nil)
+        fake-logger (fn [log-info] (reset! state_ log-info))
+        handler (bntk/wrap-request-start-logger identity {:log-fn fake-logger})]
+    (handler {:request-method :get :uri "/foo" :query-string "bar=2"})
+    (t/is (= {:level :info
+             :msg "Started GET '/foo?bar=2'"
+             :attrs {:method "GET" :path "/foo?bar=2"}}
+             @state_))))
