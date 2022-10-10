ecdc98d60d68f1699eddbcd31f2375642a41c7b6
Bob Nadler <bnadlerjr@gmail.com>
2022-10-10 10:34:37 -0400

    Ring middleware for wrapping a request ID

--- src/com/bobnadler/toolkit.clj
+++ src/com/bobnadler/toolkit.clj
@@ -1,6 +1,23 @@
-(ns com.bobnadler.toolkit)
+(ns com.bobnadler.toolkit
+  "Various utility functions for Clojure projects.")
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (prn x "Hello, World!"))
+(defn wrap-request-id
+  "Ring middleware that wraps a request with an identifier.
+
+  Checks the request for a `x-request-id` header, if one is present it is
+  added to the request map. If not present one is generated and added to the
+  request map."
+  [handler]
+  (fn [request]
+    (let [request-id (get-in request [:headers "x-request-id"] (java.util.UUID/randomUUID))]
+      (handler (assoc request :request-id (.toString request-id))))))
+
+(comment
+  (def handler (wrap-request-id identity))
+
+  (handler {}) ; {:request-id "2c8d4796-dc7a-47db-8182-f172b1f06521"}
+
+  (handler {:headers {"x-request-id" "c9bd7379-3fe9-45e8-8f69-a8e20fa007a7"}})
+; {:headers {"x-request-id" "c9bd7379-3fe9-45e8-8f69-a8e20fa007a7"},
+;  :request-id "c9bd7379-3fe9-45e8-8f69-a8e20fa007a7"}
+  )
--- test/com/bobnadler/toolkit_test.clj
+++ test/com/bobnadler/toolkit_test.clj
@@ -1,7 +1,14 @@
 (ns com.bobnadler.toolkit-test
-  (:require [clojure.test :refer :all]
-            [com.bobnadler.toolkit :refer :all]))
+  (:require [clojure.test :as t]
+            [com.bobnadler.toolkit :as btk]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 1 1))))
+(t/deftest wrap-request-id-test
+  (let [handler (btk/wrap-request-id identity)]
+
+    (t/testing "no header"
+      (t/is (contains? (handler {}) :request-id)))
+
+    (t/testing "header is present"
+      (let [req-id "c9bd7379-3fe9-45e8-8f69-a8e20fa007a7"]
+        (t/is (= req-id
+                 (:request-id (handler {:headers {"x-request-id" req-id}}))))))))
